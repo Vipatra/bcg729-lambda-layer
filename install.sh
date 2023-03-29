53810041133d94b4f92ac5f0e86af3d60e2d189d
Ranjith <ranjith19@gmail.com>
2023-03-29 17:12:45 +0530

    fix dir

--- install.sh
+++ install.sh
@@ -10,9 +10,18 @@ cmake . -DCMAKE_INSTALL_PREFIX=g729 -DCMAKE_PREFIX_PATH=g729 -DENABLE_TESTS=YES
 make
 make install
 
+rm -rf executables
 
-mkdir executables
+libdir=executables/lib
+exedir=executables/bin
+
+mkdir -p $libdir
+mkdir -p $exedir
+
 exe=test/encoderTest
+
+cp $exe $exedir
+
 
 echo "Collecting the shared library dependencies for test/encoderTest"
 deps=$(ldd $exe | awk 'BEGIN{ORS=" "}$1\
@@ -24,8 +33,8 @@ echo "Copying the dependencies to executables"
 #Copy the deps
 for dep in $deps
 do
-    echo "Copying $dep to executables"
-    cp "$dep" "executables"
+    echo "Copying $dep to $libdir"
+    cp "$dep" "$libdir"
 done
 
 echo "Done!"
