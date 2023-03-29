9554def9e83804bea59c6ad591667f48a478f260
Ranjith <ranjith19@gmail.com>
2023-03-29 16:59:28 +0530

    wip

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt-get -y install openjpeg-devel libjpeg-devel fontconfig-devel libtiff-devel libpng-devel xz gcc gcc-c++ epel-release zip cmake3
+sudo apt-get -y install gcc make cmake
 
 cmake . -DCMAKE_INSTALL_PREFIX=g729 -DCMAKE_PREFIX_PATH=g729 -DENABLE_TESTS=YES
 make
