ad272fb684e2ca9edc1c00e2cfa4cc8e3a060779
Ranjith <ranjith19@gmail.com>
2023-03-29 17:02:05 +0530

    git reset

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@
 sudo apt update
 sudo apt-get -y install gcc make cmake build-essential
 
+git reset --hard
+
 cmake . -DCMAKE_INSTALL_PREFIX=g729 -DCMAKE_PREFIX_PATH=g729 -DENABLE_TESTS=YES
 make
 make install
