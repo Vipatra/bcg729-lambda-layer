0f4962079834949644430ddd0706c0b10c7cacd3
Ranjith <ranjith19@gmail.com>
2023-03-29 17:00:00 +0530

    add build essential

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt-get -y install gcc make cmake
+sudo apt-get -y install gcc make cmake build-essential
 
 cmake . -DCMAKE_INSTALL_PREFIX=g729 -DCMAKE_PREFIX_PATH=g729 -DENABLE_TESTS=YES
 make
